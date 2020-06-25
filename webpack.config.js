525f73fa2403053cbe7b48f464a25f54d3981e9b
leonmezu1 <leo.mez@outlook.com>
2020-06-25 11:46:19 -0500

    Fixed loader package for production

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,11 @@ module.exports = {
         exclude: /node_modules/,
       },
       {
+        test: /\.svg/,
+        use: 'file-loader?name=[name].[ext]&outputPath=./images/',
+        exclude: /node_modules/,
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: 'file-loader?name=[name].[ext]&outputPath=./images/',
         exclude: /node_modules/,
